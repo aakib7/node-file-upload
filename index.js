942b4c6e73cd625f189bd89cdfdd15d3bb87e10f
aakib7 <ajmehdi5@gmail.com>
2022-09-18 17:07:54 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/users");
+    },
+    filename: function (req, file, callBack) {
+      callBack(null, `${Date.now() + file.originalname.split(" ").join("-")}`);
+    },
+  }),
+}).single("users");
+
+app.post("/post", upload, async (req, res) => {
+  console.log(req.file);
+  return res.send("Uploaded");
+});
+
+app.listen(4000);
